7c1b08c158faa646650e6f25a4b144dd88602535
Antonio Fantoni <antonio.b.fantoni@gmail.com>
2024-11-13 16:50:46 -0300

    commit 4

---
+++ src/main/java/io/github/antonio/fantoni/arquiteturaSpring/montadora/Montadora.java
@@ -0,0 +1,6 @@
+package io.github.antonio.fantoni.arquiteturaSpring.montadora;
+
+public enum Montadora {
+    HONDA,
+    TOYOTA
+}
--- src/main/java/io/github/antonio/fantoni/arquiteturaSpring/montadora/TipoMotor.java
+++ src/main/java/io/github/antonio/fantoni/arquiteturaSpring/montadora/TipoMotor.java
@@ -1,6 +1,10 @@
 package io.github.antonio.fantoni.arquiteturaSpring.montadora;
+
 /*ara situações em que você quer criar um conjunto de constantes com
 um número fixo de valores bem definidos, como estações do ano, status de pedidos, ou dias da semana.*/
 
 public enum TipoMotor {
+    ASPIRADO,
+    TURBO,
+    ELETRICO
 }
