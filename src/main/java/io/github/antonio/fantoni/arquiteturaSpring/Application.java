4cce3daf40ce0055ea1db49ea70514e1b5aa7b46
Antonio Fantoni <antonio.b.fantoni@gmail.com>
2024-11-13 16:47:25 -0300

    commit 1

--- src/main/java/io/github/antonio/fantoni/arquiteturaSpring/Application.java
+++ src/main/java/io/github/antonio/fantoni/arquiteturaSpring/Application.java
@@ -1,11 +1,7 @@
 package io.github.antonio.fantoni.arquiteturaSpring;
 
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-
-import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class Application {
---
+++ src/main/java/io/github/antonio/fantoni/arquiteturaSpring/montadora/TipoMotor.java
@@ -0,0 +1,6 @@
+package io.github.antonio.fantoni.arquiteturaSpring.montadora;
+/*ara situações em que você quer criar um conjunto de constantes com
+um número fixo de valores bem definidos, como estações do ano, status de pedidos, ou dias da semana.*/
+
+public enum TipoMotor {
+}
